389303f91e7ddb4fce9b54a15be50f8c26ec0073
‘vindesktop’ <vincentr0309@gmail.com>
2024-05-30 16:01:02 -0400

    QoL

--- folder2directionpaths.py
+++ folder2directionpaths.py
@@ -4,31 +4,35 @@ import json
 from dir_crawler import dir_crawler
 import os
 import sys
+# import kml_writer, view_edit
+import argparse
+# import pdb
+
+parser = argparse.ArgumentParser(
+    prog="Folder to KML",
+    description="Turns folder of jsons into a KML file with a path that shows the direction each point is looking in"
+)
+
+parser.add_argument('-i',
+                    '--input',
+                    help="Path to metadata folder",
+                    metavar="",
+                    action="store")
+
+parser.add_argument('-o',
+                    '--outfile',
+                    help="Write output kml to this path",
+                    metavar="",
+                    action="store")
+
+args = parser.parse_args()
 
 # files = dir_crawler(r'C:\msys64\home\didi\kml_writer\stevens_challenge_dataset_4\stevens_challenge_dataset_4\reference\ta2_metadata','.json',None)
 # files = dir_crawler(r'D:\240325-siteRTX0002-delivery\siteRTX002-SIT-sampus\siteRTX0002-SIT-campus\camA001-ZED2-2K-1\2024-01-31-13-14-31', '.json', None)
-files = dir_crawler(sys.argv[1], '.json', None)
+files = dir_crawler(args.input, '.json', None)
 jsons = files.traverse()
-# f = open(jsons[i])
-# data = json.load(f)
-# lon1 = data['extrinsics']['lon']
-# lat1 = data['extrinsics']['lat']
-# kap = data['extrinsics']['kappa']
-# f.close()
 
-# lat2, lon2 = calculate_new_point(lat1, lon1, 0.02, kap)
-
-# # kml goes lon, lat, alt
-
-# print(lon1)
-# print(lat1)
-# print(kap)
-# print(lon2)
-# print(lat2)
-
-# out = kml_writer("kmltestcombo.kml")
-
-out = kml_writer("kmltestcombo.kml", os.path.basename(os.path.dirname(jsons[0])))
+out = kml_writer(args.outfile, os.path.basename(os.path.dirname(jsons[0])))
 
 for jsun in jsons:
     f = open(jsun)
